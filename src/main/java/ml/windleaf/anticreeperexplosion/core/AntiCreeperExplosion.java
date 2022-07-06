81c0f08cbefbb79575adecfa0b4a4d945010e99b
WindLeaf233 <mc.windleaf@foxmail.com>
2022-07-06 17:36:25 +0800

    :construction: initial logger and version

--- src/main/java/ml/windleaf/anticreeperexplosion/core/AntiCreeperExplosion.java
+++ src/main/java/ml/windleaf/anticreeperexplosion/core/AntiCreeperExplosion.java
@@ -1,17 +1,26 @@
 package ml.windleaf.anticreeperexplosion.core;
 
 import ml.windleaf.api.logging.PluginLogger;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static org.bukkit.ChatColor.*;
 
 public final class AntiCreeperExplosion extends JavaPlugin {
 
+    public static PluginLogger logger = new PluginLogger(AntiCreeperExplosion.class.getSimpleName());
+    public static String version = "1.0.0";
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        logger.setLoggerColor(GREEN);
+        logger.logConsole("%sLoading %sAntiCreeperExplosion%s...".formatted(GREEN, DARK_GREEN, GREEN));
+
+        logger.logConsole("%sYou are on version %s%s".formatted(GREEN, AQUA, version));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
